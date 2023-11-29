c57c94f2bc20883c8a018beb27cf759aa95dc24a
schwoisi <aschwals@htl-steyr.ac.at>
2023-11-29 11:12:39 +0100

    Added Win conditions for all Classes

--- src/main/java/com/example/ssp/Scissor.java
+++ src/main/java/com/example/ssp/Scissor.java
@@ -1,4 +1,10 @@
 package com.example.ssp;
 
 public class Scissor extends GameElement{
+    //Setting Win Conditions
+    Scissor(){
+        wstone = 2;
+        wscissor=0;
+        wpaper = 1;
+    }
 }
--- src/main/java/com/example/ssp/Stone.java
+++ src/main/java/com/example/ssp/Stone.java
@@ -1,4 +1,10 @@
 package com.example.ssp;
 
 public class Stone extends GameElement{
+    //Setting Win Conditions
+    Stone(){
+        wstone = 0;
+        wscissor=1;
+        wpaper = 2;
+    }
 }
