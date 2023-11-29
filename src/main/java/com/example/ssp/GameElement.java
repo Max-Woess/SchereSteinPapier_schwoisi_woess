952afbc87198330ea70f88c4dbfb121d41237a44
schwoisi <aschwals@htl-steyr.ac.at>
2023-11-29 11:07:46 +0100

    Added all necessary Variables to Class GameElement

--- src/main/java/com/example/ssp/GameElement.java
+++ src/main/java/com/example/ssp/GameElement.java
@@ -1,4 +1,11 @@
 package com.example.ssp;
 
 public abstract class GameElement {
+    //int w_objectname 0... Draw
+    //                 1... Win
+    //                 2... Lose
+
+    protected int wstone;
+    protected int wpaper;
+    protected  int wscissor;
 }
