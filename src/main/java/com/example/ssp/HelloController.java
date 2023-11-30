c488c3309a8d8e40408c7db07ed766b58e7fbd44
schwoisi <aschwals@htl-steyr.ac.at>
2023-11-30 08:26:27 +0100

    All Controller Variables added

--- src/main/java/com/example/ssp/HelloController.java
+++ src/main/java/com/example/ssp/HelloController.java
@@ -1,8 +1,12 @@
 package com.example.ssp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
     //Generates Pick of the Computer
@@ -13,6 +17,53 @@ public class HelloController {
 
         return (int)(Math.random() * 3 - 1 + 1) + 1;
     }
+    //8 imagviews, 4Buttons, VBox
+    @FXML
+    VBox vbox;
+    @FXML
+    Button scissor;
+    @FXML
+    Button rock;
+    @FXML
+    Button paper;
+    @FXML
+    Button win;
+    @FXML
+    Button lose;
+    @FXML
+    Button select;
+    @FXML
+    ImageView scissoroffimg;
+    @FXML
+    ImageView scissoronimg;
+    @FXML
+    ImageView rockoffimg;
+    @FXML
+    ImageView rockonimg;
+    @FXML
+    ImageView paperoffimg;
+    @FXML
+    ImageView paperonimg;
+    @FXML
+    ImageView lossimg;
+    @FXML
+    ImageView winimg;
+    @FXML
+    ImageView coinimg;
+    @FXML
+    ImageView backgroundimg;
+    @FXML
+    Label yourpick;
+    @FXML
+    Label compick;
+    @FXML
+    Label coincount;
+
+
+
+
+
+
 
 
 
